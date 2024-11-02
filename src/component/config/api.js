022ee3fcdf84636ec8889d4ad88764aa876c7019
Silvery86 <giang8692@gmail.com>
2024-11-02 21:28:30 +0700

    Update: Api URL

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URI = "http://localhost:5454"
+export const API_URI = process.env.REACT_APP_API_URI || "http://localhost:5454"
 
 export const api = axios.create({
     baseURL:API_URI,
