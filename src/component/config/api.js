806c08bbf11398fe368243656642ca1564d3b8d7
Silvery86 <giang8692@gmail.com>
2024-11-02 21:54:37 +0700

    Update: API URL

--- src/component/config/api.js
+++ src/component/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const isProduction = window.location.hostname === 'foodsou.store';
-export const API_URI = isProduction ? "https://foodsou.store/api/" : "http://localhost:5454";
+export const API_URI = isProduction ? "https://foodsou.store" : "http://localhost:5454";
 
 export const api = axios.create({
     baseURL:API_URI,
